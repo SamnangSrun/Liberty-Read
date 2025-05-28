9f6ec2843f69119cbdd65064cf934504f7fd0ecd
nang <you@example.com>
2025-05-28 14:01:14 +0700

    Fix typo in Categories.jsx import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Hero from "./Components/Index/Hero.jsx";
-import ServiceCard from "./Components/Index/ServiceCard.jsx";
-import Categories from "./Components/index/Categoriess.jsx";
-import Anime from "./Components/Index/Anime-Films/Anime.jsx";
-import Khmer from "./Components/Index/KhmerLegend/khmer.jsx";
-import Feedbacks from "./Components/Index/Feedback.jsx";
-import Questions from "./Components/Index/Questions.jsx";
+import Hero from "@/Components/Index/Hero.jsx";
+import ServiceCard from "@/Components/Index/ServiceCard.jsx";
+import Categories from "@/Components/index/Categories.jsx";
+import Anime from "@/Components/Index/Anime-Films/Anime.jsx";
+import Khmer from "@/Components/Index/KhmerLegend/khmer.jsx";
+import Feedbacks from "@/Components/Index/Feedback.jsx";
+import Questions from "@/Components/Index/Questions.jsx";
 
 
 function App() {
