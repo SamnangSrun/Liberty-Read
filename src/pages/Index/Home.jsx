d258546a3cce2c3c985ceca84a640089a0bbcf5e
nang <you@example.com>
2025-05-28 14:29:16 +0700

    Fix alias or case-sensitive path

--- src/pages/Index/Home.jsx
+++ src/pages/Index/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import HeroHome from '../../Components/Index/HeroHome'
+import HeroHome from '@/Components/Index/HeroHome'
 
-import ServiceCard from "../../Components/Index/ServiceCard.jsx";
-import Categories from "../../Components/index/Categories.jsx";
-import Anime from "../../Components/Index/Anime-Films/Anime.jsx";
-import Khmer from "../../Components/Index/KhmerLegend/khmer.jsx";
-import Feedbacks from "../../Components/Index/Feedback.jsx";
-import Questions from "../../Components/Index/Questions.jsx";
+import ServiceCard from "@/Components/Index/ServiceCard.jsx";
+import Categories from "@/Components/index/Categories.jsx";
+import Anime from "@/Components/Index/Anime-Films/Anime.jsx";
+import Khmer from "@/Components/Index/KhmerLegend/khmer.jsx";
+import Feedbacks from "@/Components/Index/Feedback.jsx";
+import Questions from "@/Components/Index/Questions.jsx";
 
 const Home = () => {
   return (
