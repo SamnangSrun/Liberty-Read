bad82303b725ffbb8b3ec531b9baa59d4d3a5346
nang <srunsamnang2003@gmail.com>
2025-06-09 21:48:36 +0700

    :truck:....

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 export const config = {
   base_url_api: "https://liberty-read-api-production.up.railway.app/api/",
-  profile_image_path: "https://liberty-read-api-production.up.railway.app/storage/",
-  book_image_path: "https://liberty-read-api-production.up.railway.app/storage/", // add more if needed
+  profile_image_path: "https://api.cloudinary.com/v1_1/daoka4qqr/image/upload",
+  book_image_path: "https://api.cloudinary.com/v1_1/daoka4qqr/image/upload", // add more if needed
 };
