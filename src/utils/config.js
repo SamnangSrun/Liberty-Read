1ed52c77f359b48ab425daf90d8c61771065b4d4
nang <srunsamnang2003@gmail.com>
2025-06-10 21:08:36 +0700

    new route1

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 export const config = {
   // base_url_api: "https://liberty-read-api-production.up.railway.app/api/",
-    base_url_api: "http://liberty.books.publicvm.com/api",
+    base_url_api: "http://liberty.books.publicvm.com/api/",
 
   profile_image_path: "http://liberty.books.publicvm.com/storage/",
   book_image_path: "http://liberty.books.publicvm.com/storage/", // add more if needed
