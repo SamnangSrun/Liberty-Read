ea6fc865562c802a338a9c8dead099aef5c67d97
nang <srunsamnang2003@gmail.com>
2025-06-10 21:12:23 +0700

    new route2

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
 export const config = {
   // base_url_api: "https://liberty-read-api-production.up.railway.app/api/",
-    base_url_api: "http://liberty.books.publicvm.com/api/",
+    base_url_api: "https://liberty.books.publicvm.com/api/",
 
-  profile_image_path: "http://liberty.books.publicvm.com/storage/",
-  book_image_path: "http://liberty.books.publicvm.com/storage/", // add more if needed
+  profile_image_path: "https://liberty.books.publicvm.com/storage/",
+  book_image_path: "https://liberty.books.publicvm.com/storage/", // add more if needed
 };
