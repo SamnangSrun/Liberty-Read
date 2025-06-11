b4e3b0721e0627f08ea699a1820687b4da2a625b
nang <srunsamnang2003@gmail.com>
2025-06-12 00:11:50 +0700

    new i

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,8 @@
 export const config = {
-  base_url_api: "https://test-api-production-dc1a.up.railway.app/api/",
-  cloudinary_upload_url: "https://api.cloudinary.com/v1_1/daoka4qqr/image/upload",
-  cloudinary_base_url: "https://res.cloudinary.com/daoka4qqr/image/upload/"
+  // base_url_api: "https://liberty-read-api-production.up.railway.app/api/",
+    // base_url_api: "https://liberty.books.publicvm.com/api/",
+    base_url_api: "https://test-api-production-dc1a.up.railway.app/api/",
+
+  profile_image_path: "https://api.cloudinary.com/v1_1/daoka4qqr/image/upload",
+  book_image_path: "https://test-api-production-dc1a.up.railway.app/storage/", // add more if needed
 };
