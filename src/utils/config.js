36faa42e7d76189d9593c5ce08728346c87113fa
nang <srunsamnang2003@gmail.com>
2025-06-12 12:27:08 +0700

    done

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,8 @@
 export const config = {
   // base_url_api: "https://liberty-read-api-production.up.railway.app/api/",
     // base_url_api: "https://liberty.books.publicvm.com/api/",
-    base_url_api: "https://test-api-production-dc1a.up.railway.app/api/",
+    base_url_api: "https://test-api-production-dc1a.up.railway.app/",
 
-  profile_image_path: "https://res.cloudinary.com/daoka4qqr/image/upload",
-  book_image_path: "https://test-api-production-dc1a.up.railway.app/storage/", // add more if needed
+  profile_image_path: "",
+  book_image_path: "", // add more if needed
 };
