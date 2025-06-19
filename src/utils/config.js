3774f936f4e55a7173b55325670a32a8b8a79e37
nang <srunsamnang2003@gmail.com>
2025-06-19 17:17:40 +0700

    .

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
 export const config = {
   // base_url_api: "https://liberty-read-api-production.up.railway.app/api/",
     // base_url_api: "http://127.0.0.1:8000/api/",
-    base_url_api: "https://test-api-production-dc1a.up.railway.app/",
+    base_url_api: "https://test-api-production-dc1a.up.railway.app/api/",
 
   profile_image_path: "",
   book_image_path: "", // add more if needed
